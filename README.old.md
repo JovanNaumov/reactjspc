faf82a07095ee675cf0d34f894521b7c400208d9
JovanNaumov1 <mm_jovce@hotmail.com>
2022-12-14 22:08:00 +0100

    cas1

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# reactjspc
+
+
+## initial
---
+++ src/components/Student.js
@@ -0,0 +1,23 @@
+
+const Student = (props) => {
+    
+    // ternarent
+
+    if (props.souldTheComponentShow) {
+        console.log("true")
+    }else {
+        console.log("false")
+    }
+
+    props.souldTheComponentShow ? console.log("true") : console.log("false")
+    // console.log(props)
+    return <div>
+        <h1>Student</h1>
+        <div>Name : {props.name}</div>
+        <div>Age : {props.age}</div>
+        <div>ShouldShow : {props.shouldTheComponentShow}</div>
+
+    </div>
+}
+
+export default Student
